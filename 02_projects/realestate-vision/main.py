a7e6bb0be9a5c29b98ebf88477157d63fc49fc55
Kristina110205 <nazdrachevasa@gmail.com>
2026-03-30 12:50:19 +0300

    Add files via upload

---
+++ 02_projects/realestate-vision/main.py
@@ -0,0 +1,6 @@
+import config
+
+print(f" Город: {config.CITY_NAME}")
+print(f" Валюта: {config.CURRENCY}")
+print(f"🖼 Размер фото: {config.IMG_SIZE}x{config.IMG_SIZE}")
+print(f" Конфигурация загружена!")
